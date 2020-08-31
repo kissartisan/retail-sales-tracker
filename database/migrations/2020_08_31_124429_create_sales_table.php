040027240f797de0a238d94cb526bfcb0c785e48
Reymark Torres <reymark.torres08.dev@gmail.com>
2020-08-31 20:47:46 +0800

    Create sales migration

--- database/migrations/2020_08_31_124429_create_sales_table.php
+++ database/migrations/2020_08_31_124429_create_sales_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateSalesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained();
-            $table->string('name');
-            $table->decimal('supplier_price', 8, 2);
-            $table->decimal('selling_price', 8, 2);
+            $table->string('code')->unique();
+            $table->foreignId('barangay_id')->constrained();
+            $table->decimal('grand_total', 8, 2);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('sales');
     }
 }
