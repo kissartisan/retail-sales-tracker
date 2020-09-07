be12e406675ad89eaf4b7eca321509e231bcba69
Reymark Torres <reymark.torres08.dev@gmail.com>
2020-09-07 14:42:58 +0800

    Add categories on the nav bar & create a resource controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li>
+                            <a class="nav-link" href="{{ route('categories.index') }}">Categories</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('categories', CategoryController::class);
 Route::get('/home', 'HomeController@index')->name('home');
